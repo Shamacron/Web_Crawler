11f2115a4bed3eb0e47a03df65e05ec40ee3a5ec
BrownSoldier <Isaiahcjc5@hotmail.com>
2017-07-27 15:53:15 -0600

    New outline Download please i need a life in this "College"

--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -1,5 +1,3 @@
-import java.net.URL;
-import java.util.List;
 import java.util.regex.Pattern;
 
 //edited 7-26-17
@@ -9,8 +7,12 @@ public class WebCrawler{
 	 * Main Method Uses args to check if the strings are urls if not it thorws a illegal 
 	 * argument excetion stating that it the arguments are not vaild urls.
 	 */
-	public static void main(String[] args) {
-		checkString(args);
+	public static void main(String[] args) 
+	{
+		if(checkString(args)) 
+		{
+		CralWeb(args);
+		}
 	}
 	
 	
@@ -18,10 +20,13 @@ public class WebCrawler{
 	/*
 	 * Checks if the array of strings are vaild urls
 	 */
-	public static boolean checkString(String[] urls) {
-		String regex = "";
-		for(int i = 0 ;i < urls.length; i++) {
-			if(Pattern.matches(regex, urls[i])) {
+	public static boolean checkString(String[] urls) 
+	{
+		String regex = "^((http[s]?|ftp):\\/)?\\/?([^:\\/\\s]+)((\\/\\w+)*\\/)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$";
+		for(int i = 0 ;i < urls.length; i++) 
+		{
+			if(Pattern.matches(regex, urls[i])) 
+			{
 				return true;
 			}
 			else 
@@ -29,4 +34,64 @@ public class WebCrawler{
 		}
 		return false;
 	}
+	
+	/*
+	 * Implents all the web craling stuff and does the things that do 
+	 * the other things
+	 */
+	public static void CralWeb(String[] Urls) 
+	{
+		
+	}
+	/*
+	 * finds webpages that are exteinos of the local page
+	 */
+	public static String[] getInternalWebPages() 
+	{
+		return null;
+		
+	}
+	/*
+	 * Finds webpages that are not extions of the local page
+	 */
+	public static String[] getExternalWebPages()
+	{
+		return null;
+		
+	}
+	/*
+	 * uses regex to get the text inside the body of the html
+	 */
+	public static String getBodyText()
+	{
+		return null;
+		
+	}
+	/*
+	 * uses regex to retun the text inside of the <p> tag into an array of strings for diffrent p tags
+	 */
+	public static String getPText()
+	{
+		return null;
+	}
+	/*
+	 * uses regex to return the text inside of the <div tag>
+	 */
+	public static String getDivText()
+	{
+		return null;
+		
+	}
+	/*
+	 * get for li tag
+	 */
+	public static String getLiText()
+	{
+		return null;
+		
+	}
+	/*
+	 * get for label tag
+	 */
+	public static String get
 }
